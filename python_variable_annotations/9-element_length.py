1b10c2630a9198f0d7b538fd3073f19ad49e58f9
GuillermoRosario <guillermorosario628@gmail.com>
2023-08-31 16:13:19 -0400

    PEP8 Verification

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -2,7 +2,7 @@
 """ Basic Python Annotations """
 
 
-from typing import List, Tuple, Iterable, Sequence 
+from typing import List, Tuple, Iterable, Sequence
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
