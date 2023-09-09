52a6c6e077b297fcda3803407bdd19676c4a3dce
GuillermoRosario <guillermorosario628@gmail.com>
2023-09-08 21:29:45 -0400

    Code Fix

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
-"""Script that return a list of school having
-a specific topic"""
+"""
+MongoDB manipulation in Python
+"""
 import pymongo
 
 
-def schools_by_topic(mongo_collection, topic: str) -> list:
-    """Return a list of schools"""
-    sch: list = []
-    query = {"topics": topic}
-
-    for school in mongo_collection.find(query):
-        sch.append(school)
-
-    return sch
+def update_topics(mongo_collection, name, topics):
+    """
+    Changes all topics of document based on name
+    """
+    new_doc = mongo_collection.update_many({
+        "name" : name
+        },
+        {
+            "$set": {
+                "topics": topics
+                }
+            })
+    return new_doc
