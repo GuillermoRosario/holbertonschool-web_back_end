f63a230e3c0b45d9e2fd186cf60d490c6e9194d1
GuillermoRosario <guillermorosario628@gmail.com>
2023-10-12 23:28:01 -0400

    correction

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,6 @@
 export default function getsListStudentIds(array) {
-    if (Array.isArray(array)) {
-      return array.map((student) => student.id);
-    }
-    return [];
+  if (Array.isArray(array)) {
+    return array.map((student) => student.id);
+  }
+  return [];
 }
--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,13 +1,13 @@
 export default function updateUniqueItems(_map) {
-    if (!(_map instanceof Map)) {
-      throw Error('Cannot process');
+  if (!(_map instanceof Map)) {
+    throw Error('Cannot process');
+  }
+
+  for (const [key] of _map) {
+    if (_map.get(key) === 1) {
+      _map.set(key, 100);
     }
-  
-    for (const [key] of _map) {
-      if (_map.get(key) === 1) {
-        _map.set(key, 100);
-      }
-    }
-  
-    return _map;
+  }
+
+  return _map;
 }
